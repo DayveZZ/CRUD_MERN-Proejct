a308fb8bb987686c8692c015711e97f2a8db2887
Abhijit Deb <abhijitdebofficial@gmail.com>
2025-10-23 21:00:20 +0530

    router

--- server/routes/workout.Router.js
+++ server/routes/workout.Router.js
@@ -3,3 +3,10 @@ import Workout from "../models/Workout.Model.js";
 const router = express.Router();
 
 // Create a new workout
+router("/", async (req, res) => {
+  try {
+    const workoutData = await Workout.find().sort({ createdAt: -1 });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
