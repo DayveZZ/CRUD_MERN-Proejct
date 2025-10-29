43f7a3f4debdfcac378603c588cac81a05df6b60
Abhijit Deb <abhijitdebofficial@gmail.com>
2025-10-29 17:34:31 +0530

    get data to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  return <div>App</div>;
+  const [workouts, setWorkouts] = useState(null);
+
+  const port = import.meta.env.VITE_PORT;
+
+  const getWorkouts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:${port}/api/workouts`);
+      setWorkouts(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching workouts:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getWorkouts();
+  }, []);
+
+  return (
+    <div>
+      {workouts &&
+        workouts.map((i) => {
+          return (
+            <div key={i._id}>
+              <h1>{i.title}</h1>
+              <h1>{i.reps}</h1>
+              <h1>{i.load}</h1>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default App;
